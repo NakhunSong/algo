c3972d8fb913aaf2bca336a08b060c6c8b194d30
NakhunSong <nak4627@gmail.com>
2019-05-10 13:43:43 +0900

    Add backjoon_11722

---
+++ algorithm/dp/Backjoon_11722/Backjoon_11722.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n+1];
+		int[] d = new int[n+1];
+		for(int i=1; i<=n; i++) {
+			a[i] = sc.nextInt();
+		}
+		for(int i=n; i>=1; i--) {
+			d[i] = 1;
+			for(int j=i+1; j<=n; j++) {
+				if(a[j] < a[i] && d[i] < d[j]+1) {
+					d[i] = d[j] + 1;
+				}
+			}
+		}
+		int ans = d[1];
+		for(int i=2; i<=n; i++) {
+			ans = Math.max(ans, d[i]);
+		}
+		System.out.println(ans);
+	}
+
+}
