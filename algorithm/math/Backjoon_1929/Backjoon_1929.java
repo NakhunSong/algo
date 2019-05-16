a0b12cae34d09bc6f6e56211dc240d51bee9b1fa
NakhunSong <nak4627@gmail.com>
2019-05-16 23:54:50 +0900

    Add backjoon_1929

---
+++ algorithm/math/Backjoon_1929/Backjoon_1929.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		boolean[] check = new boolean[m+1];
+		check[0] = check[1] = true;
+		for(int i=2; i<=m; i++) {
+			if(check[i] == true) {
+				continue;
+			}
+			for(int j=i*2; j<=m; j+=i) {
+				check[j] = true;
+			}
+		}
+		for(int i=n; i<=m; i++) {
+			if(check[i] == false) {
+				System.out.println(i);
+			}
+		}
+	} 
+
+}
