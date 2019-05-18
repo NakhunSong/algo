36859d25a714396d37b7a40b3d34f43c4a420c64
NakhunSong <nak4627@gmail.com>
2019-05-18 23:24:20 +0900

    Add backjoon_11652

---
+++ algorithm/sort/Backjoon_11652/Backjoon_11652.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		long[] a = new long[n];
+		for(int i=0 ;i<n; i++) {
+			a[i] = Long.parseLong(br.readLine());
+		}
+		Arrays.sort(a);
+		long ans = a[0];
+		int ans_cnt = 1;
+		int cnt = 1;
+		for(int i=1; i<n; i++) {
+			if(a[i-1] != a[i]) {
+				cnt = 1;
+			} else {
+				cnt += 1;
+			}
+			if(cnt > ans_cnt) {
+				ans_cnt = cnt;
+				ans = a[i];
+			}
+		}
+		System.out.println(ans_cnt);
+	}
+
+}
