503eab953defc5ab868b6e80e531c3572ce19222
NakhunSong <nak4627@gmail.com>
2019-05-28 23:07:41 +0900

    Fix b_11057

--- algorithm/dp/Backjoon_11057/Backjoon_11057.java
+++ algorithm/dp/Backjoon_11057/Backjoon_11057.java
@@ -1,25 +1,32 @@
-import java.util.Scanner;
+package backjoon.dp;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long mod = 10007;
-		long[][] d = new long[n+1][10];
-		for(int i=0; i<=9; i++) d[1][i] = 1;
+public class B_11057 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int mod = 10007;
+		int n = Integer.parseInt(br.readLine());
+		int[][] d = new int[n+1][10];
+		for(int i=0; i<=9; i++) {
+			d[1][i] = 1;
+		}
 		for(int i=2; i<=n; i++) {
 			for(int j=0; j<=9; j++) {
+				d[i][j] = 0;
 				for(int k=0; k<=j; k++) {
-					d[i][j] += d[i-1][k];					
-					d[i][j] %= mod;
+					d[i][j] += d[i-1][k];
 				}
+				d[i][j] %= mod;
 			}
 		}
-		long ans = 0;
-		for(int i=0; i<10; i++) ans += d[n][i];
-		ans %= mod;
-		System.out.println(ans);
+		int ans = 0;
+		for(int i=0; i<=9; i++) {
+			ans += d[n][i];
+		}
+		System.out.println(ans%mod);
 	}
 
 }
