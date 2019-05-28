375ae1d0a8a596c8af3101f3660c7ce545b46f75
NakhunSong <nak4627@gmail.com>
2019-05-28 23:06:46 +0900

    Fix b_9095

--- algorithm/dp/Backjoon_9095/Backjoon_9095.java
+++ algorithm/dp/Backjoon_9095/Backjoon_9095.java
@@ -1,24 +1,24 @@
+package backjoon.dp;
+
 import java.util.Scanner;
 
-public class Main {
-    public static int[] dp = new int[11];
-    public static int go(int n) {
-        if(n<0) return 0;
-        if(n<=1) return 1;
-        if(dp[n]>0) return dp[n];
-        dp[n] = go(n-1) + go(n-2) + go(n-3);
-        return dp[n];
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        int[] ans = new int[t];
-        for(int i=0; i<t; i++) {
-            int n = sc.nextInt();
-            ans[i] = go(n);
-        }
-        for(int i=0; i<t; i++) {
-            System.out.println(ans[i]);
-        }
-    }
+public class B_9095 {
+	public static int[] d;
+	public static int go(int n) {
+		if(n<0) return 0;
+		if(n <= 1) return 1;
+		if(d[n] > 0) return d[n];
+		d[n] = go(n-1) + go(n-2) + go(n-3);
+		return d[n];
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0) {
+			int n = sc.nextInt();
+			d = new int[n+1];
+			System.out.println(go(n));
+		}
+	}
+
 }
