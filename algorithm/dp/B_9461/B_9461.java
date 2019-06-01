e88a29746439eee32d2bd6592975f2c5a7fdae90
NakhunSong <nak4627@gmail.com>
2019-06-01 16:48:54 +0900

    Fix b_9461

---
+++ algorithm/dp/B_9461/B_9461.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0) {
+			int n = sc.nextInt();
+			long[] d = new long[n+1];
+			d[1] = 1;
+			if(n>=2) {
+				d[2] = 1;
+			}
+			if(n>=3) {
+				d[3] = 1;
+			}
+			for(int i=4; i<=n; i++) {
+				d[i] = d[i-2] + d[i-3];
+			}
+			System.out.println(d[n]);
+		}
+	}
+
+}
