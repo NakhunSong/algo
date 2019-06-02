c6f433f12c71d721f041ac0f1ea945f3cfc55baa
NakhunSong <nak4627@gmail.com>
2019-06-02 22:26:41 +0900

    Add b_1152

---
+++ algorithm/string/B_1152/B_1152.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine().trim();
+		StringTokenizer st = new StringTokenizer(s);
+		System.out.println(st.countTokens());
+	}
+
+}
