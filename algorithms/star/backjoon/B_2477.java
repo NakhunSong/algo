c7a6fa8e210c9060a112afaa64517b0d5b25da22
NakhunSong <nak4627@gmail.com>
2019-07-25 23:53:23 +0900

    Add backjoon_2477

---
+++ algorithms/star/backjoon/B_2477.java
@@ -0,0 +1,28 @@
+package backjoon.star;
+
+import java.util.Scanner;
+
+public class B_2447 {
+   public static void go(int n, int x, int y) {
+      if((x/n)% 3 == 1 && (y/n)%3 == 1) {
+         System.out.print(" ");
+      } else {
+         if(n/3 == 0) {
+            System.out.print("*");
+         } else {
+            go(n/3, x, y);
+         }
+      }
+   }
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      for(int i=0; i<n; i++) {
+         for(int j=0; j<n; j++) {
+            go(n, i, j);            
+         }
+         System.out.println();
+      }
+   }
+
+}
