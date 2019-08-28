a727588ee1696594294645ceac716f9b815b140e
NakhunSong <nak4627@gmail.com>
2019-08-29 00:28:15 +0900

    Update brute-force

--- algorithms/backjoon/Brute_force/B_9019.java
+++ algorithms/backjoon/Brute_force/B_9019.java
@@ -1,32 +1,8 @@
-// Correct but timeout
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	public static int change(int now, String command) {
-		String[] s = Integer.toString(now).split("");
-		String num = "";
-		if(command.equals("L")) {
-			String temp = s[0];
-			for(int i=1; i<s.length; i++) {
-				s[i-1] = s[i];
-				num+=s[i-1];
-			}
-			s[s.length-1] = temp;			
-			num+=s[s.length-1];
-		} else {
-			String temp = s[s.length-1];
-			for(int i=0; i<s.length-1; i++) {
-				s[i+1] = s[i]; 
-				num+=s[i+1];
-			}
-			s[0] = temp;
-			num+=s[0];
-		}
-		
-		return Integer.parseInt(num);
-	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
@@ -42,40 +18,36 @@ public class Main {
 			d[a] = "";
 			while(!q.isEmpty()) {
 				int now = q.remove();
-				int L = change(now, "L");
-				if(check[L] == false) {
-					check[L] = true;
-					d[L] = d[now] + "L";
-					q.add(L);
-				}
-				int R = change(now, "R");
-				if(check[R] == false) {
-					check[R] = true;
-					d[R] = d[now] + "R";
-					q.add(R);
-				}
-				int D = 0;
-				if(now*2 > 9999) {
-					D = (now*2)%10000;
-				} else {
-					D = now*2;
-				}
+
+				int D = (now*2)%10000;
 				if(check[D] == false) {
 					q.add(D);
 					d[D] = d[now] + "D"; 
 					check[D] = true;
 				}
-				int S = 0;
-				if(now-1 <= 0) {
-					S = 9999;
-				} else {
-					S = now-1;
-				}
+
+				int S = now-1;
+				if(S == -1) S = 9999;
 				if(check[S] == false) {
 					q.add(S);
 					d[S] = d[now] + "S";
 					check[S] = true;
 				}
+				
+				int L = (now%1000)*10 + now/1000;
+				if(check[L] == false) {
+					check[L] = true;
+					d[L] = d[now] + "L";
+					q.add(L);
+				}
+
+				int R = (now/10) + (now%10)*1000;
+				if(check[R] == false) {
+					check[R] = true;
+					d[R] = d[now] + "R";
+					q.add(R);
+				}
+
 			}
 			System.out.println(d[b]);
 		}
